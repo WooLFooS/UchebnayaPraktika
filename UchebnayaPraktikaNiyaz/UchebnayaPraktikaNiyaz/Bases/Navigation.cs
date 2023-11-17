203089f9e0ea7f1b1472745f54dbd6831e752241
Niyaz <nfaizrahmanov05@gmail.com>
2023-11-17 18:01:56 +0300

    Primer1

---
+++ UchebnayaPraktikaNiyaz/UchebnayaPraktikaNiyaz/Bases/Navigation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace UchebnayaPraktikaNiyaz.Bases
+{
+    static class Navigation
+    {
+        private static List<PageComponent> components = new List<PageComponent>();
+        public static MainWindow mainWindow;
+
+        public static void ClearHistory()
+        {
+            App.isAdmin = false;
+            components.Clear();
+        }
+        private static void Update(PageComponent pageComponent)
+        {
+            mainWindow.NameLb.Content = pageComponent.Title;
+            mainWindow.ExitBtn.Visibility = components.Count() > 1 ? System.Windows.Visibility.Visible : System.Windows.Visibility.Hidden;
+            mainWindow.BackBtn.Visibility = App.isAdmin ? System.Windows.Visibility.Visible : System.Windows.Visibility.Hidden;
+            mainWindow.MainFrame.Navigate(pageComponent.Page);
+        }
+        public static void NextPage(PageComponent pageComponent)
+        {
+            components.Add(pageComponent);
+            Update(pageComponent);
+
+        }
+        public static void BackPage()
+        {
+            if (components.Count > 1)
+            {
+                components.RemoveAt(components.Count - 1);
+                Update(components[components.Count - 1]);
+            }
+        }
+    }
+
+    class PageComponent
+    {
+        public string Title { get; set; }
+        public Page Page { get; set; }
+        public PageComponent(string title, Page page)
+        {
+            Title = title;
+            Page = page;
+        }
+    }
+
+}
