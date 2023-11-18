6e40b8f1cab5aadf2b97774f9d4562c2da3c3ee5
Niyaz <nfaizrahmanov05@gmail.com>
2023-11-18 17:01:21 +0300

    practika

---
+++ UchebnayaPraktikaNiyaz/UchebnayaPraktikaNiyaz/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using System.Windows;
+using UchebnayaPraktikaNiyaz.Bases;
+
+namespace UchebnayaPraktikaNiyaz
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static UchebnayaPraktikaNiyazEntities db = new UchebnayaPraktikaNiyazEntities();
+        public static bool isAdmin = false;
+        public static bool isStudent = false;
+    }
+}
---
+++ UchebnayaPraktikaNiyaz/UchebnayaPraktikaNiyaz/Pages/AuthorizatePage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using UchebnayaPraktikaNiyaz.Bases;
+
+
+namespace UchebnayaPraktikaNiyaz.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorizatePage.xaml
+    /// </summary>
+    public partial class AuthorizatePage : Page
+    {
+        public AuthorizatePage()
+        {
+            InitializeComponent();
+        }
+
+        private void EnterBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var emp = App.db.Employee.Where(x => x.Id_Employee.ToString() == PasswordPb.Password).FirstOrDefault();
+            var std = App.db.Student.Where(x => x.Id_Student.ToString() == PasswordPb.Password).FirstOrDefault();
+            if(emp != null)
+            {
+                App.isAdmin = true;
+                MessageBox.Show("Good Morning, Admin");
+                Navigation.NextPage(new PageComponent("Студент", new StudentPage()));
+
+            }
+            else if(std != null)
+            {
+                App.isStudent = true;
+                MessageBox.Show("Good Morning, Student");
+                Navigation.NextPage(new PageComponent("Студент", new StudentPage()));
+            }
+            else
+            {
+                MessageBox.Show("Sorry, there is no such account");
+            }
+        }
+    }
+}
