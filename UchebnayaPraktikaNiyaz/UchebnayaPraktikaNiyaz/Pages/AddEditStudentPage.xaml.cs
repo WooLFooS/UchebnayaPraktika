e800e2731d3752fe444452628f7341827575c671
Niyaz <nfaizrahmanov05@gmail.com>
2023-11-21 13:11:47 +0300

    HomeWork

--- UchebnayaPraktikaNiyaz/UchebnayaPraktikaNiyaz/Pages/AddEditStudentPage.xaml.cs
+++ UchebnayaPraktikaNiyaz/UchebnayaPraktikaNiyaz/Pages/AddEditStudentPage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using UchebnayaPraktikaNiyaz.Bases;
 
 namespace UchebnayaPraktikaNiyaz.Pages
 {
@@ -20,9 +21,42 @@ namespace UchebnayaPraktikaNiyaz.Pages
     /// </summary>
     public partial class AddEditStudentPage : Page
     {
-        public AddEditStudentPage()
+        public Student student;
+        public string WhatToDo;
+        public AddEditStudentPage(Student _student, string _whatToDo)
         {
             InitializeComponent();
+            student = _student;
+            WhatToDo = _whatToDo;
+            this.DataContext = student;
+
+        }
+
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            bool errors = false;
+            var selectSpec = SpecCb.SelectedItem as Specs;
+            if(selectSpec == null || FIOTb.Text == "")
+            {
+                errors = true;
+                MessageBox.Show("Заполните все данные");
+            }
+            if(!errors)
+            {
+                if (WhatToDo == "add")
+                {
+                    App.db.Student.Add(new Student()
+                    {
+                        Surname_Student = student.Surname_Student,
+                        Id_Spec = selectSpec.Id_Spec,
+                        IsDeleted = Convert.ToBoolean(0)
+                    });
+                }
+                else student.Id_Spec = selectSpec.Id_Spec;
+                MessageBox.Show("Сохранено!");
+                App.db.SaveChanges();
+                NavigationService.Navigate(new StudentPage());
+            }
         }
     }
 }
--- UchebnayaPraktikaNiyaz/UchebnayaPraktikaNiyaz/Pages/StudentPage.xaml.cs
+++ UchebnayaPraktikaNiyaz/UchebnayaPraktikaNiyaz/Pages/StudentPage.xaml.cs
@@ -28,15 +28,18 @@ namespace UchebnayaPraktikaNiyaz.Pages
         public StudentPage()
         {
             InitializeComponent();
-            StudentList.ItemsSource = App.db.Student.ToList();
+            Refresh();
             if(!App.isAdmin)
             {
                 AddBtn.Visibility = Visibility.Hidden;
+                DeleteBtn.Visibility = Visibility.Hidden;
+                RedaktBtn.Visibility = Visibility.Hidden;
             }
+            
         }
 
 
-        private void Refresh()
+        public void Refresh()
         {
             IEnumerable<Student> SortedStudent = App.db.Student;
             if (SortList.SelectedIndex == 0)
@@ -64,7 +67,7 @@ namespace UchebnayaPraktikaNiyaz.Pages
                 SortedStudent = SortedStudent.Where(x => x.Surname_Student.ToLower().Contains(SearchTb.Text.ToLower()));
             }
 
-            StudentList.ItemsSource = SortedStudent.ToList();
+            StudentList.ItemsSource = SortedStudent.ToList().Where(x => x.IsDeleted != Convert.ToBoolean(1));
         }
        
         private void SortList_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -89,7 +92,10 @@ namespace UchebnayaPraktikaNiyaz.Pages
 
         private void DeleteBtn_Click(object sender, RoutedEventArgs e)
         {
-            
+            var student = (Student)StudentList.SelectedItem;
+            student.IsDeleted = Convert.ToBoolean(1);
+            Refresh();
+            App.db.SaveChanges();
         }
 
         private void RedaktBtn_Click(object sender, RoutedEventArgs e)
